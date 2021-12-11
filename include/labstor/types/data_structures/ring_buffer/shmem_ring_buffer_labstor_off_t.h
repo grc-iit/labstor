3d2531b0806d6c89f521c2ded73834b0f1515167
lukemartinlogan <lukemartinlogan@gmail.com>
2021-12-10 18:19:10 -0600

    Make sure that bit is set before reserving in ring buffer

--- include/labstor/types/data_structures/ring_buffer/shmem_ring_buffer_labstor_off_t.h
+++ include/labstor/types/data_structures/ring_buffer/shmem_ring_buffer_labstor_off_t.h
@@ -134,33 +134,20 @@ static inline bool labstor_ring_buffer_labstor_off_t_Enqueue_simple(struct labst
     return labstor_ring_buffer_labstor_off_t_Enqueue(rbuf, data, &enqueued);
 }
 
-static inline bool labstor_ring_buffer_labstor_off_t_WaitForSet(struct labstor_ring_buffer_labstor_off_t *rbuf, uint32_t bit) {
-    int max_retries = 10000, i = 0;
-    while(!labstor_bitmap_IsSet(rbuf->bitmap_, bit) && i < max_retries) { ++i; LABSTOR_YIELD(); }
-    return i < max_retries;
-}
-
 static inline bool labstor_ring_buffer_labstor_off_t_Dequeue(struct labstor_ring_buffer_labstor_off_t *rbuf, labstor_off_t *data) {
     uint32_t enqueued, dequeued;
     uint32_t entry;
     do {
         enqueued = rbuf->header_->enqueued_;
         dequeued = rbuf->header_->dequeued_;
-        if(enqueued < dequeued) { return false; }
-        if(enqueued == dequeued) { return false; }
+        if(enqueued <= dequeued) { return false; }
+        entry = dequeued % rbuf->header_->max_depth_;
+        if(!labstor_bitmap_IsSet(rbuf->bitmap_, entry)) { return false; }
     }
     while(!__atomic_compare_exchange_n(&rbuf->header_->dequeued_, &dequeued, dequeued + 1, false, __ATOMIC_RELAXED, __ATOMIC_RELAXED));
-    entry = dequeued % rbuf->header_->max_depth_;
-    if(labstor_ring_buffer_labstor_off_t_WaitForSet(rbuf, entry)) {
-        *data = rbuf->queue_[entry];
-        labstor_bitmap_Unset(rbuf->bitmap_, entry);
-        return true;
-    } else {
-        *data = rbuf->queue_[entry];
-        labstor_bitmap_Unset(rbuf->bitmap_, entry);
-        TRACEPOINT("For some reason, a ring buffer's data was invalid. Process may have died during enqueue.\n");
-        return false;
-    }
+    *data = rbuf->queue_[entry];
+    labstor_bitmap_Unset(rbuf->bitmap_, entry);
+    return true;
 }
 
 #ifdef __cplusplus
