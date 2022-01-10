1e4e9b08df4cbe5158686fde31329dca36422dc1
lukemartinlogan <lukemartinlogan@gmail.com>
2022-01-09 20:49:24 -0600

    Some small integer maths

--- Untitled Folder/src/userspace/client/client.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by lukemartinlogan on 8/4/21.
-//
-
-#include <labstor/userspace/client/client.h>
-
--- Untitled Folder/src/userspace/server/work_balancer.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by lukemartinlogan on 12/4/21.
-//
-
-#include <labstor/userspace/server/server.h>
-
-void labstor::Server::WorkBalancer::DoWork() {
-    sleep(5);
-}
