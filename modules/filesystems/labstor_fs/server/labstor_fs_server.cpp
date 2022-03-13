bf61fc0e1b0107627f8b3998cd92de3f5a05959b
lukemartinlogan <lukemartinlogan@gmail.com>
2022-03-13 18:35:37 -0500

    LabFS log

--- modules/filesystems/labstor_fs/server/labstor_fs_server.cpp
+++ modules/filesystems/labstor_fs/server/labstor_fs_server.cpp
@@ -31,20 +31,30 @@ inline void labstor::LabFS::Server::Initialize(labstor::ipc::request *rq) {
     labstor::Module *module;
     register_request *reg_rq = reinterpret_cast<register_request*>(rq);
     next_module_ = namespace_->Get(reg_rq->next_);
+
+    //Load head table from disk
+
+    //Load all
 }
 inline void labstor::LabFS::Server::Open(labstor::queue_pair *qp, labstor::GenericPosix::open_request *client_rq, labstor::credentials *creds) {
-    //Get INODE by fd
-    //Complete the I/O request
+    //Allocate file UUID
+    //Map file path to UUID
+    //Append to log
 }
 inline void labstor::LabFS::Server::Close(labstor::queue_pair *qp, labstor::GenericPosix::close_request *client_rq, labstor::credentials *creds) {
-    //Unmark inode as busy
-    //Complete the I/O request
+    //sync all data & metadata back to storage
 }
 inline void labstor::LabFS::Server::IOStart(labstor::queue_pair *qp, labstor::GenericPosix::io_request *client_rq, labstor::credentials *creds) {
-    //Allocate free blocks on the device (per-core ring buffer of 4KB pages)
-    //Store the mappings
+    size_t total_io = client_rq->size_;
+    for(size_t cur_io = 0; cur_io < total_io; ) {
+        //Allocate a block from the device
+        //Create request designated to the next module
+        //Send block request to the next module
+        //Append (IO_START, file_off, block)
+    }
 }
 inline void labstor::LabFS::Server::IOComplete(labstor::queue_pair *qp, labstor::GenericPosix::io_request *client_rq, labstor::credentials *creds) {
+    //Append IO_END block request to log
 }
 
 LABSTOR_MODULE_CONSTRUCT(labstor::LabFS::Server, LABFS_MODULE_ID)
